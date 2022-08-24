5d98a9d7270bce6c54b7ebec0510b4ea83893a3f
johnsonisaacpeter <ijohnson@student.neumont.edu>
2022-08-24 12:02:55 -0600

    Initial Commit

--- neo4j.py
+++ neo4j.py
@@ -6,16 +6,3 @@ graph = Graph(password="mypassword")
 # results = graph.run("match (p:People) return p limit 50")
 # for node in results.data():
 #     print(node['p'].get('firstName'))
-
-#1
-# node50 = graph.nodes.match(id = "50").first()
-# print(node50)
-# boss50 = graph.match(nodes=[node50], r_type="BOSS_OF").count()
-# print(boss50)
-
-#2
-# node666 = graph.nodes.match(id = "666").first()
-# print(node666)
-# boss666 = graph.match(nodes=[node666], r_type="BOSS_OF").count()
-# print(boss666)
-
