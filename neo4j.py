b0cde0b9df9134ecde5dd6cd04083baf7e0d45cd
Memesterr <98419522+Memesterr@users.noreply.github.com>
2022-08-24 20:04:56 -0600

    Added layout for createEmployee function

--- neo4j.py
+++ neo4j.py
@@ -6,3 +6,16 @@ graph = Graph(password="mypassword")
 # results = graph.run("match (p:People) return p limit 50")
 # for node in results.data():
 #     print(node['p'].get('firstName'))
+
+# More example code:
+# Create:
+def createEmployee():
+    query = "CREATE (a:Employee {}), {}"
+    results = graph.run(query)
+    print("createEmployee Results -> " + results)
+
+# Read:
+
+# Update:
+
+# Delete:
